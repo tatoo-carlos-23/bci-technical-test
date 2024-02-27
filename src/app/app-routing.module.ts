e18f4e18c077f694dc076bde7f566be27adde397
Carlos Abramonte <cabramon@emeal.nttdata.com>
2024-02-27 18:49:11 -0500

    vista creada del home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'manager',
-    pathMatch: 'full'
-  },
+  // {
+  //   path: '',
+  //   redirectTo: 'manager',
+  //   pathMatch: 'full'
+  // },
   {
     path: '',
     loadChildren: () => import("./modules/home/home.module").then(r => r.HomeModule)
