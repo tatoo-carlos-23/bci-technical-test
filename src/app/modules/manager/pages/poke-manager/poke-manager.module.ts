876a1550756218d7fc1c60e92d669b23118ace7e
Carlos Abramonte <cabramon@emeal.nttdata.com>
2024-02-27 19:28:50 -0500

    avance de estructura de poke manager

--- src/app/modules/manager/pages/poke-manager/poke-manager.module.ts
+++ src/app/modules/manager/pages/poke-manager/poke-manager.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PokeManagerRoutingModule } from './poke-manager-routing.module';
 import { PokeManagerComponent } from './poke-manager.component';
+import { TableComponent } from './components/table/table.component';
+import { AddUpdateComponent } from './components/add-update/add-update.component';
 
 
 @NgModule({
   declarations: [
-    PokeManagerComponent
+    PokeManagerComponent,
+    TableComponent,
+    AddUpdateComponent
   ],
   imports: [
     CommonModule,
